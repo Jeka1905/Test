27577e90ccd92f50af1d1d8f996193e639c07ad7
Evgen Dorofieiev <jekateterev@gmail.com>
2024-05-12 12:40:39 +0300

    Обновлены данные в файле index.html

---
+++ project/index.js
@@ -0,0 +1,18 @@
+function scrollToMatches() {
+  var matchesBlock = document.getElementById("matches-news");
+  if (matchesBlock) {
+      matchesBlock.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+      });
+  }
+}
+function scrollToMatches() {
+  var matchesBlock = document.getElementById("teams");
+  if (matchesBlock) {
+      matchesBlock.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+      });
+  }
+}
