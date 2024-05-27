3052690c329e4079eb1a87e262a0e050c2330524
Evgen Dorofieiev <jekateterev@gmail.com>
2024-05-27 12:32:56 +0300

    Add example.txt file

--- project/index.js
+++ project/index.js
@@ -1,18 +1,39 @@
-function scrollToMatches() {
-  var matchesBlock = document.getElementById("matches-news");
+function scrollToTableContainer() {
+  var matchesBlock = document.getElementById("table-container");
   if (matchesBlock) {
-      matchesBlock.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
-      });
+    matchesBlock.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
   }
 }
-function scrollToMatches() {
+
+function scrollToTeams() {
   var matchesBlock = document.getElementById("teams");
   if (matchesBlock) {
-      matchesBlock.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
-      });
+    matchesBlock.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
   }
 }
+
+function scrollToLeaguetable() {
+  var matchesBlock = document.getElementById("Leaguetable");
+  if (matchesBlock) {
+    matchesBlock.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
+}
+
+function changeLeague() {
+  var selectedLeague = document.getElementById("league-selector").value;
+  var tables = document.querySelectorAll('.league-table-content');
+  tables.forEach(function(table) {
+      table.style.display = 'none';
+  });
+  document.getElementById(selectedLeague + '-table').style.display = 'table';
+}
+document.getElementById("ukraine-table").style.display = 'table';
